b11184894ccc068cf7826901f0bc1443d6297ab1
donutscientist <howardscordova@gmail.com>
2026-02-22 18:20:42 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,23 +1,30 @@
 const express = require('express');
 const app = express();
-app.use(express.text({ type: '*/*' }));
 
-app.post('/starcloudprnt', (req, res) => {
-  console.log("PRINTER POLLED");
-
-  const receipt = `
-^XA
-^FO50,50^ADN,36,20^FDHELLO HOWARDS DONUTS^FS
-^XZ
-`;
-
-  res.set('Content-Type', 'text/plain');
-  res.set('X-Star-CloudPRNT-Job', 'true');
-  res.status(200).send(receipt);
-});
+app.use(express.raw({ type: '*/*' }));
 
 app.get('/', (req, res) => {
   res.send('OK');
 });
 
-app.listen(process.env.PORT || 3000);
+app.post('/starcloudprnt', (req, res) => {
+  console.log("🖨️ PRINTER POLLED");
+
+  const receipt = `
+^XA
+^FO50,50^ADN,36,20^FDHOWARD'S DONUTS TEST^FS
+^XZ
+`;
+
+  res.set({
+    "Content-Type": "text/plain",
+    "X-Star-CloudPRNT-Job": "true",
+    "X-Star-CloudPRNT-StatusCode": "200"
+  });
+
+  res.send(receipt);
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server running");
+});
