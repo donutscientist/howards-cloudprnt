cba706eea2e7bdd6bc5c507b281c2763c9ef9cb4
donutscientist <howardscordova@gmail.com>
2026-02-22 20:26:02 -0600

    Update server.js

--- server.js
+++ server.js
@@ -3,12 +3,30 @@ const app = express();
 
 app.use(express.raw({ type: '*/*' }));
 
-app.get('/', (req, res) => {
-  res.send('OK');
+let job = null;
+
+// TEST route to manually create job
+app.get('/createjob', (req, res) => {
+
+  job = Buffer.from([
+    0x1b, 0x40,
+    0x1b, 0x61, 0x01,
+    0x1b, 0x21, 0x30,
+    0x48, 0x6f, 0x77, 0x61, 0x72,
+    0x64, 0x27, 0x73, 0x20, 0x44,
+    0x6f, 0x6e, 0x75, 0x74, 0x73,
+    0x0a,
+    0x0a,
+    0x1b, 0x64, 0x03,
+    0x1d, 0x56, 0x00
+  ]);
+
+  console.log("JOB CREATED");
+  res.send("Job created");
 });
 
-let jobPending = true;
 
+// Printer polls this
 app.post('/starcloudprnt', (req, res) => {
 
   console.log("PRINTER POLLED");
@@ -16,32 +34,30 @@ app.post('/starcloudprnt', (req, res) => {
   res.setHeader("Content-Type", "application/json");
 
   res.send({
-    "jobReady": true,
-    "mediaTypes": ["application/vnd.star.starprnt"],
-    "jobToken": "12345"
+    jobReady: job !== null,
+    mediaTypes: ["application/vnd.star.starprnt"],
+    jobToken: "12345"
   });
-
 });
 
+
+// Printer downloads job here
 app.get('/starcloudprnt', (req, res) => {
 
-  console.log("PRINTER REQUESTED JOB");
+  if(job){
 
-  const job = Buffer.from([
-    0x1b, 0x40,                   // Initialize
-    0x1b, 0x61, 0x01,             // Center align
-    0x1b, 0x21, 0x30,             // Double size
-    0x48, 0x6f, 0x77, 0x61, 0x72,
-    0x64, 0x27, 0x73, 0x20, 0x44,
-    0x6f, 0x6e, 0x75, 0x74, 0x73,
-    0x0a,
-    0x0a,
-    0x1b, 0x64, 0x03,             // Feed 3
-    0x1d, 0x56, 0x00              // Cut
-  ]);
+    console.log("PRINTER REQUESTED JOB");
 
-  res.setHeader("Content-Type", "application/vnd.star.starprnt");
-  res.send(job);
+    res.setHeader("Content-Type", "application/vnd.star.starprnt");
+    res.send(job);
+
+    job = null;   // 🔥🔥🔥 THIS STOPS INFINITE PRINTING
+
+  } else {
+
+    res.status(204).send();
+
+  }
 });
 
 app.listen(process.env.PORT || 3000, () => {
