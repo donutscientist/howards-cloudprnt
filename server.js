9e218cc355165e279afc221685a69d608156eaf8
donutscientist <howardscordova@gmail.com>
2026-02-22 20:11:52 -0600

    Update server.js

--- server.js
+++ server.js
@@ -25,17 +25,21 @@ app.get('/starcloudprnt', (req, res) => {
 
   console.log("PRINTER REQUESTED JOB");
 
-  const receipt = Buffer.from([
-    0x1B, 0x40,              // Initialize printer
-    0x1B, 0x61, 0x01,        // Center
-    ...Buffer.from("HOWARD'S DONUTS\n\n"),
-    0x1B, 0x61, 0x00,        // Left align
-    ...Buffer.from("TEST PRINT SUCCESS\n\n"),
-    0x1D, 0x56, 0x41, 0x10   // Cut paper
+  const job = Buffer.from([
+    0x1b, 0x40,                   // Initialize
+    0x1b, 0x61, 0x01,             // Center align
+    0x1b, 0x21, 0x30,             // Double size
+    0x48, 0x6f, 0x77, 0x61, 0x72,
+    0x64, 0x27, 0x73, 0x20, 0x44,
+    0x6f, 0x6e, 0x75, 0x74, 0x73,
+    0x0a,
+    0x0a,
+    0x1b, 0x64, 0x03,             // Feed 3
+    0x1d, 0x56, 0x00              // Cut
   ]);
 
-  res.setHeader("Content-Type", "application/octet-stream");
-  res.status(200).send(receipt);
+  res.setHeader("Content-Type", "application/vnd.star.starprnt");
+  res.send(job);
 });
 
 app.listen(process.env.PORT || 3000, () => {
